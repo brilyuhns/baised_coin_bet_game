e7fd5d3bb7149571daff89519734bad05728e546
Divya Kunnath <kunnathdivyal@gmail.com>
2016-10-26 19:53:51 +0530

    Add classes for game and strategies

--- game.rb
+++ game.rb
@@ -1,40 +1,84 @@
-@space = [0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
-def outcome
-  @space.sample
-end
+class Game
 
-
-def play_game strategy, bet_amount
-  balance = 25
-  strategy.each do |bet|
-    cur_outcome = outcome
-    puts bet: bet
-    if bet == cur_outcome
-      balance += bet_amount
-    else
-      balance -= bet_amount
-    end
-    puts "#{{ cur_outcome: cur_outcome, bet: bet, balance: balance }}"
-    break if balance <= 0
+  attr_accessor :balance, :bet_amount
+  def initialize(strategy)
+    @space = [0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+    @balance = 25
+    @bets = []
+    @outcomes = []
+    @strategy = strategy
   end
-  puts ">>>>>>>>Final Balance in game: #{balance} <<<<<<<<<<<<"
-  balance
+
+  def outcome
+    @space.sample
+  end
+
+  def play
+    300.times do
+      bet = @strategy.next_bet(@bets, @outcomes)
+      bet_amount = @strategy.next_bet_amount(@bets, @outcomes)
+      current_outcome = outcome
+      @bets << bet
+      @outcomes << current_outcome
+      if bet == current_outcome
+        @balance += bet_amount
+      else
+        @balance -= bet_amount
+      end
+      # puts "#{{ cur_outcome: current_outcome, bet: bet, balance: @balance }}"
+      break if @balance <= 0
+    end
+  end
+
+  def balance
+    @balance
+  end
 end
 
-def strategy1
-  Enumerator.new{|a| 300.times{a << 1} }
+class Strategy1
+  def next_bet(bets, outcomes)
+    1
+  end
+
+  def next_bet_amount(bets, outcomes)
+    5
+  end
 end
 
-play_game(strategy1, 5)
-i=0
 
-data_for_100_games = 100.times.map{ i+=1; puts "Game #{i}"; play_game(strategy1, 5) }
-lost_games = data_for_100_games.select{|a| a<= 0}.count
-puts "Lost #{lost_games} out of 100"
-average = data_for_100_games.inject(0){|sum, a| sum+= a} / 100.0
-puts "Average balance of 100 games #{average}"
+game = Game.new(Strategy1.new)
+game.play
+puts game.balance
+
+class Strategy2
+   def next_bet(bets, outcomes)
+    return 1 if bets.count == 0
+    Rational(bets.count(1)/bets.count) > 0.6 ? 1 : 0
+  end
+
+  def next_bet_amount(bets, outcomes)
+    5
+  end
+end
+
+game = Game.new(Strategy2.new)
+game.play
+puts game.balance
 
 
-# def strategy2 game_so_far
+100_games_strategy1 =
 
-# end
+
+def run_for_hundred_games strategy
+  games = 100.times.map do
+    game = Game.new(Strategy1.new)
+    game.play
+    game.balance
+  end
+  lost_games = games.select{|a| a < 0}
+  puts "lost games #{lost_games}"
+  avg_balance = games.inject(0){|sum, i| sum+=1 }
+  puts "avg_balance: #{avg_balance}"
+  puts "min balance: games.min"
+  puts "max balance: games.max"
+end
